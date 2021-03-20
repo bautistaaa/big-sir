1c6ad0b8b1d30ae613051d1e81864e5354ae8aab
chris-bautista <chris.bautista@hulu.com>
2021-03-19 17:16:48 -0700

    chore: fix min/max of terminal

--- src/App.tsx
+++ src/App.tsx
@@ -4,33 +4,76 @@ import Dock from './components/Dock';
 import Terminal from './components/Terminal';
 import styled from 'styled-components/macro';
 import useRect from './hooks/useRect';
+type DraggableData = {
+  node: HTMLElement;
+  x: number;
+  y: number;
+  deltaX: number;
+  deltaY: number;
+  lastX: number;
+  lastY: number;
+};
 
 const App: FC = () => {
   const [isTerminalMinimized, setIsTerminalMinized] = useState(false);
   const terminalRef = useRef(null);
   const minimizedTerminalRef = useRef(null);
-  const [test, setTest] = useState(0);
+  const [refresh, setRefresh] = useState(0);
   const [deltaX, setDeltaX] = useState(0);
-
   const minimizedRect = useRect(minimizedTerminalRef, []);
-  const terminalRect = useRect(terminalRef, [test]);
+  const terminalRect = useRect(terminalRef, [refresh]);
+  const [deltas, setDeltas] = useState<DraggableData>();
+  const [addTransition, setAddTransition] = useState(false);
+  const [position, setPosition] = useState<
+    { x: number; y: number } | undefined
+  >(undefined);
 
   useEffect(() => {
     const x = minimizedRect.left - terminalRect.left;
-    setDeltaX(x)
-    console.log(window.innerWidth)
+    setDeltaX(x);
   }, [minimizedRect, terminalRect]);
+
+  useEffect(() => {
+    if (!isTerminalMinimized) {
+      setAddTransition(true);
+      const to = setTimeout(() => {
+        setAddTransition(false);
+      }, 700);
+
+      return () => {
+        clearTimeout(to);
+      };
+    }
+  }, [isTerminalMinimized]);
+
+  useEffect(() => {
+    if (isTerminalMinimized) {
+      setPosition({
+        x: deltaX,
+        y: 1000,
+      });
+    } else {
+      if (deltas) {
+        setPosition({ x: deltas!.lastX, y: deltas!.lastY });
+      } else {
+        setPosition(undefined);
+      }
+    }
+  }, [isTerminalMinimized, deltas]);
+
+  const handleOnStop = (_: any, d: DraggableData) => {
+    setDeltas(d);
+    setRefresh((x) => x + 1);
+  };
 
   return (
     <Wrapper>
       <Overlay />
-      <Draggable
-        onStop={() => setTest((x) => x + 1)}
-        position={isTerminalMinimized ? { x: deltaX, y: 1000 } : undefined}
-      >
+      <Draggable onStop={handleOnStop} position={position}>
         <TerminalWrapper
           ref={terminalRef}
           isTerminalMinimized={isTerminalMinimized}
+          addTransition={addTransition}
         >
           <Terminal setIsTerminalMinimized={setIsTerminalMinized} />
         </TerminalWrapper>
@@ -61,8 +104,9 @@ const Overlay = styled.div`
 `;
 const TerminalWrapper = styled.div<{
   isTerminalMinimized: boolean;
+  addTransition: boolean;
 }>`
-  ${({ isTerminalMinimized }) =>
+  ${({ isTerminalMinimized, addTransition }) =>
     isTerminalMinimized
       ? `
         pointer-events: none;
@@ -70,8 +114,10 @@ const TerminalWrapper = styled.div<{
         transform: scale(0.1);
         transition: transform .7s, opacity 0.5s;
       `
-      : `opacity: 1;`
-  }
+      : `
+        ${addTransition ? 'transition: transform .7s, opacity 0.5s;' : ''}
+        opacity: 1;
+        `}
   position: absolute;
   bottom: 40%;
   left: 30%;

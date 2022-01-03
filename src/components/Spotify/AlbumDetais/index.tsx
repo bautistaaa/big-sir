fc02251e3212c4b6ac9cb4f611ac990812fd8cd6
chris-bautista <chris.bautista@hulu.com>
2022-01-02 23:20:38 -0800

    chore: kill TS errors

--- src/components/Spotify/AlbumDetais/index.tsx
+++ src/components/Spotify/AlbumDetais/index.tsx
@@ -7,22 +7,23 @@ import PlaylistTable from '../PlaylistTable';
 interface AlbumDetailsProps {}
 
 const AlbumDetais = () => {
-  return (
-    <Wrapper>
-      <PlaylistHero
-        backgroundColor={heroBackgroundColor}
-        image={imageSrc}
-        category={playlist?.type}
-        title={playlist?.name}
-        description={playlist?.description ?? ''}
-        author={playlist?.owner?.display_name ?? ''}
-        followers={playlist?.followers?.total}
-        tracksTotal={playlist?.tracks?.total}
-      />
-      <UtilityBar playlist={playlist} />
-      {items && <PlaylistTable items={items} />}
-    </Wrapper>
-  );
+  return <div />;
+  // return (
+  //   <Wrapper>
+  //     <PlaylistHero
+  //       backgroundColor={heroBackgroundColor}
+  //       image={imageSrc}
+  //       category={playlist?.type}
+  //       title={playlist?.name}
+  //       description={playlist?.description ?? ''}
+  //       author={playlist?.owner?.display_name ?? ''}
+  //       followers={playlist?.followers?.total}
+  //       tracksTotal={playlist?.tracks?.total}
+  //     />
+  //     <UtilityBar playlist={playlist} />
+  //     {items && <PlaylistTable items={items} />}
+  //   </Wrapper>
+  // );
 };
 
 const Wrapper = styled.div`

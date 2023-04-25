7515bd596bd2957b551f5c4f3ef2f364cc7a4331
Vincent Chavez <vincent@vin-e.com>
2023-04-25 16:58:37 -0700

    update process.env with vite

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 // <reference types"vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_REACT_APP_SPOTIFY_CLIENT_ID: string;
+  readonly VITE_REACT_APP_SPOTIFY_CLIENT_SECRET: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
